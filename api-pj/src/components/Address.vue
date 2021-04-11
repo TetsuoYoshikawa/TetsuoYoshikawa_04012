1eb898910fba086811d484da810695d6c3dea598
tetsuoYoshikawa <bball.t3y18@gmail.com>
2021-04-11 22:37:30 +0900

    frist

---
+++ api-pj/src/components/Address.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <input v-model="address" type="text"/>
+    <button @click="getAddress">住所自動入力</button>
+    <p>Address :{{address}}</p>
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ api-pj/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <Address/>
+  </div>
+</template>
+
+<script>
+import Address from "../components/Address";
+export default{
+  components:{
+    Address
+  }
+};
+
+</script>
