49380bea848824d73241e36ac55187cfe0556725
TetsuoYoshikawa <77278965+TetsuoYoshikawa@users.noreply.github.com>
2021-04-12 11:44:53 +0900

    Update Address.vue

--- api-pj/src/components/Address.vue
+++ api-pj/src/components/Address.vue
@@ -2,10 +2,23 @@
   <div class="home">
     <input v-model="address" type="text"/>
     <button @click="getAddress">住所自動入力</button>
-    <p>Address :{{address}}</p>
+    <p>Address :{{showAddress}}</p>
   </div>
 </template>
 
 <script>
-
+export default{
+  data(){
+    return{
+      address:"",
+      showAddress:""
+    };
+  },
+  methods:{
+    getAddress(){
+      const item = await aixos.get('https://apis.post code-is.com/api/v3/post codes?postcode=${this.address});
+      const addressData = item.data;
+      this.showAddress = allAddress;
+  }
+};
 </script>
